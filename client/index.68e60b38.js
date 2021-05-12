5c0d7a787b3ef3d22269bf076bf68893104d56df
LetonGerman <neveraskedfor@gmail.com>
2021-05-12 12:44:03 +0300

    Updates

---
+++ client/index.68e60b38.js
@@ -0,0 +1,1 @@
+import{S as s,i as r,s as t,m as e}from"./client.12c25f11.js";function o(s){return e("course/frameworks"),[]}export default class extends s{constructor(s){super(),r(this,s,o,null,t,{})}}
